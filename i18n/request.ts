98a995139e29fabc12d7de66b89aa51a17ded9a8
Carlos Bezerra <carlosfilipe.lb@gmail.com>
2026-05-16 16:15:56 -0300

    fix i18n

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,19 +2,21 @@ import { getUserLocale } from '@/services/locale';
 import { getRequestConfig } from 'next-intl/server';
 import { Locale } from './config';
 
-const localeFileMap: Record<Locale, string> = {
-    'EN': 'english.json',
-    'pt-BR': 'portuguese.json',
-    'ES': 'spanish.json',
+import en from './locales/english.json';
+import ptBR from './locales/portuguese.json';
+import es from './locales/spanish.json';
+
+const messages: Record<Locale, typeof ptBR> = {
+    'EN': en,
+    'pt-BR': ptBR,
+    'ES': es,
 };
 
 export default getRequestConfig(async () => {
-    // Provide a static locale, fetch a user setting,
-    // read from `cookies()`, `headers()`, etc.
     const locale = await getUserLocale() as Locale;
 
     return {
         locale,
-        messages: (await import(`./locales/${localeFileMap[locale]}`)).default
+        messages: messages[locale],
     };
 });
